58d873e89d96c72c3bd71c420e427380ee6a282f
antoney07 <antonygabrielmor@gmail.com>
2025-12-02 23:12:21 -0300

    Getters e setters

--- src/main/java/org/model/Despesa.java
+++ src/main/java/org/model/Despesa.java
@@ -10,4 +10,43 @@ public class Despesa {
     private LocalDate data;
     private String categoria;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
