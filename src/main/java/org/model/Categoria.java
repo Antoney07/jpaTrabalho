9098cfab076f07d2bdaea44d975f0ff229aff871
antoney07 <antonygabrielmor@gmail.com>
2025-12-03 00:57:12 -0300

    tenho q admitir que fiquei perdido e pedi ajuda pro chat, continuo perdido

---
+++ src/main/java/org/model/Categoria.java
@@ -0,0 +1,24 @@
+package org.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Categoria {
+    ALIMENTACAO("Alimentação"),
+    TRANSPORTE("Transporte"),
+    LAZER("Lazer"),
+    SAUDE("Saúde"),
+    EDUCACAO("Educação"),
+    OUTROS("Outros");
+
+    private final String descricao;
+
+    Categoria(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao; // Isso faz aparecer bonitinho no ComboBox
+    }
+}
