f673b19988704c685ef7acd5d32f64d4c1058a26
antoney07 <antonygabrielmor@gmail.com>
2025-12-02 23:58:14 -0300

    usando lombok

--- src/main/java/org/model/Despesa.java
+++ src/main/java/org/model/Despesa.java
@@ -1,9 +1,13 @@
 package org.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "despesas")
 public class Despesa {
@@ -16,46 +20,6 @@ public class Despesa {
     private LocalDate data;
     private String categoria;
 
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
 
     public Despesa() {
     }
