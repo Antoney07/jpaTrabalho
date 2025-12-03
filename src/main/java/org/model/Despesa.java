dc3844e11fd12e602355cf82ee0627b98027a2ac
antoney07 <antonygabrielmor@gmail.com>
2025-12-02 23:11:38 -0300

    implementação da classe despesa e de suas variáveis

---
+++ src/main/java/org/model/Despesa.java
@@ -0,0 +1,13 @@
+package org.model;
+
+import java.time.LocalDate;
+
+public class Despesa {
+
+    private int id;
+    private String descricao;
+    private double valor;
+    private LocalDate data;
+    private String categoria;
+
+}
